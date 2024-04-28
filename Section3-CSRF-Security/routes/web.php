694364b98f3104ce0c0745777c363dffc5d2affb
sabski03 <saba27ge@gmail.com>
2024-04-28 21:24:09 +0400

    CSRF

--- Section3-CSRF-Security/routes/web.php
+++ Section3-CSRF-Security/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    $token = $request->session()->token();
+
+    return $token;
+    //return csrf_token();
+});
+
+Route::post('/posts', function (){
+    return "it worked";
 });
