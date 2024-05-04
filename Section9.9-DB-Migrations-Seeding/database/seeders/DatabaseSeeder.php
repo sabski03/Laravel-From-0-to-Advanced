80323a417d14e1099ee399e803e61292a9af2eeb
sabski03 <saba27ge@gmail.com>
2024-05-04 14:12:30 +0400

    Section9.9

--- Section9.9-DB-Migrations-Seeding/database/seeders/DatabaseSeeder.php
+++ Section9.9-DB-Migrations-Seeding/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,7 @@
 
 namespace Database\Seeders;
 
-use Faker\Factory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,18 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $faker = Factory::create();
+        $this->call([
+            UsersTableSeeder::class,
+        ]);
 
-        for ($i = 0; $i < 100; $i++){
-            DB::table('users')->insert([
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'password' => $faker->password(8),
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
+
     }
 }
---
+++ Section9.9-DB-Migrations-Seeding/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++){
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password(8),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
