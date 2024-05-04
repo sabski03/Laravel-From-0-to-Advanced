91bdc90ec0102c28df62238e4d06d92229df0899
sabski03 <saba27ge@gmail.com>
2024-05-04 13:52:40 +0400

    seeding users

--- Section9.9-DB-Migrations-Seeding/database/seeders/DatabaseSeeder.php
+++ Section9.9-DB-Migrations-Seeding/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 100; $i++){
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password(8),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
