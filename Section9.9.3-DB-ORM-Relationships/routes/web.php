9a67031eee90d7629782549fc7a8fca682f63c50
sabski03 <saba27ge@gmail.com>
2024-05-05 13:01:54 +0400

    belongsTo

--- Section9.9.3-DB-ORM-Relationships/routes/web.php
+++ Section9.9.3-DB-ORM-Relationships/routes/web.php
@@ -49,9 +49,32 @@ Route::get('/', function () {
 //    ]);
 //    User::find(1)->posts()->save($post);
 
+//    dd(User::find(1)->posts()->whereId(10)->delete());
 
 
-    dd(User::find(1)->posts()->whereId(10)->delete());
+//    $comment = Comment::find(1);
+//    $post = Post::find(12);
+//    $comment->post()->associate($post);
+//    $comment->save();
+//    Comment::find(1)->post()->associate()->save();
+
+
+//    $post = Comment::find(1)->post->title;
+//    $post = Comment::find(1)->post;
+//    $post->title = "new Title";
+//    $post->body = "new Body";
+//    $post->save();
+
+
+//    $post = Post::find(12);
+//    dd($post->user);
+//    dd($post->comments);
+
+//    $post->title = "updated from the push show post";
+//    $post->comments[0]->message = "updated from the push show comment";
+//    $post->user->name = "saba";
+//    $post->push();
+
 
     return response(200);
 
